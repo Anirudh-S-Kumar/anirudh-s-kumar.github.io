8ae3f21bd32e11450c7c4789e942a2e38aaf4883
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2023-12-21 02:55:04 +0530

    added tailwind and a sample button for test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    import Button from "$lib/shared/Button.svelte";
+
+
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button>
+    Hello
+    </Button>
+    
